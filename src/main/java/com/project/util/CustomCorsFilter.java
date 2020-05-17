80a9ab96a2d98f4e9681d59f2560790397dca154
two dog <ekfrl2815@gmail.com>
2020-05-18 00:42:08 +0900

    bb

--- src/main/java/com/project/util/CustomCorsFilter.java
+++ src/main/java/com/project/util/CustomCorsFilter.java
@@ -5,6 +5,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -36,6 +38,8 @@ public class CustomCorsFilter implements Filter {
                 "x-requested-with, authorization, Content-Type, Authorization, credential, X-AUTH-TOKEN, X-CSRF-TOKEN");
 
         logger.info("CustomCorsFilter>>>>>>");
+        logger.info("UUID>>>>>>" + ((HttpServletRequest) req).getHeader("UUID"));
+
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
         } else {
